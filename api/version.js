bbdd5fb1bc6a616729a75ccbbf6186c57ced117c
Fanky <106883384+fanky86@users.noreply.github.com>
2026-03-05 09:37:52 +0700

    Update version.js

--- api/version.js
+++ api/version.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.status(200).json({
     latestVersion: 2,
-    apkUrl: "https://domainkamu.com/Mavora.apk"
+    apkUrl: "https://github.com/fanky86/Mavora_Version/releases/download/v1.1/Mavora.apk"
   });
 }
